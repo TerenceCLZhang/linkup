dabdb958676cf1a65b844fba1cc42e4ee4f1f16e
TerenceCLZhang <terence.zhang2014@gmail.com>
2025-10-25 20:49:08 +1300

    env setup

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,8 +1,8 @@
 import express from "express";
 import morgan from "morgan";
+import { ENV } from "./config/env.js";
 
 const app = express();
-const PORT = 8080;
 
 app.use(morgan("tiny"));
 
@@ -10,6 +10,6 @@ app.use((req, res) => {
   res.send("Server is running!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
+app.listen(ENV.PORT, () => {
+  console.log(`Server is listening on port ${ENV.PORT}`);
 });
