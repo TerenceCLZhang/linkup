007f6b8e7e331214f9d28c97d1c1102f116876fd
TerenceCLZhang <terence.zhang2014@gmail.com>
2025-11-06 01:40:55 +1300

    updated skeleton

--- frontend/src/components/skeletons/ChatContainerSkeleton.tsx
+++ frontend/src/components/skeletons/ChatContainerSkeleton.tsx
@@ -1,7 +1,10 @@
 const ChatContainerSkeleton = () => {
   return (
     <div className="flex-1 flex flex-col gap-5 p-5">
-      <div className="skeleton h-10 w-100 rounded-lg" />
+      <div className="flex gap-1 items-center">
+        <div className="skeleton size-10 rounded-full" />
+        <div className="skeleton h-7 w-50" />
+      </div>
       <div className="flex-1" />
       <div className="skeleton w-full h-10" />
     </div>
